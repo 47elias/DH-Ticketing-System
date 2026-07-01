8d5cecb82fd3e9e214e9d9d4d2c5c31eb7d7f6ac
Musa Elias Mukahlera <eliyasmukahlera1971@gmail.com>
2026-07-01 12:03:50 +0200

    Created the slash screen

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DH_Ticketing_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            linkLabel1 = new LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(125, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(497, 65);
+            label1.TabIndex = 0;
+            label1.Text = "DH Ticketing System";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.buse_logo_removebg_preview;
+            pictureBox1.Location = new Point(259, 93);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(196, 212);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(313, 354);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 13);
+            label2.TabIndex = 2;
+            label2.Text = "Version V1.0.0";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(294, 377);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(122, 13);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "https://www.elias.co.zw";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 426);
+            Controls.Add(linkLabel1);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Font = new Font("Microsoft Sans Serif", 8.25F);
+            Name = "Form1";
+            Text = "Welcome Page";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private LinkLabel linkLabel1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+namespace DH_Ticketing_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string url = "https://www.elias.co.zw";
+
+            try
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open the link: " + ex.Message);
+            }
+        }
+    }
+}
